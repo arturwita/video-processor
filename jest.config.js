95645159e64408b234c6010595cf2b3973f1fd53
arturwita <witart1998@gmail.com>
2023-02-03 17:55:25 +0100

    feat: initial commit

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  testPathIgnorePatterns: [],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)?$",
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node",
+  ],
+  roots: ["<rootDir>"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      { tsconfig: "tsconfig.json", isolatedModules: true },
+    ],
+  },
+};
---
+++ src/fun.spec.ts
@@ -0,0 +1,9 @@
+import { xd } from "./fun";
+
+describe("fun", () => {
+  it("should ", function () {
+    const a = 1;
+    const b = xd();
+    expect(a).toEqual(b);
+  });
+});
